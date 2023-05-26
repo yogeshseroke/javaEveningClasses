2ce1493c7dc63a79e2a86a21abe4cbed34ceede3
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-26 20:02:10 +0530

    Add files via upload

---
+++ javaClassEvening/DataMembersExample.java
@@ -0,0 +1,116 @@
+package javaClassEvening;
+
+public class DataMembersExample {
+
+	/*
+	 * 
+	 * 		DataMembers.... static(local and global) and 
+	 * 						non-static(local and global)
+	 * 
+	 * */
+	//non-static variables...
+	//global variable...
+//	int a ; 
+//	
+//	int b ;
+	
+	//local and non-static...
+//	void accept(int a, int b) {
+//		 this.a=a;
+//		this.b=b;
+//	}
+	
+//	void display() {
+//		System.out.println(a+b);
+//	}
+	
+	
+	//==================static==========
+	
+	//static global....
+	
+//	static int a;
+
+	//static local....
+	
+//	static void accept() {
+//		a=10;
+//		
+//	}
+//	
+//	static void display() {
+//		System.out.println(a);
+//	}
+	
+	//local non-static....
+//	int a;
+//	void accept() {
+//		a=10;
+//	}
+//	
+//	void display() {
+//		System.out.println(a);
+//	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		DataMembersExample obj = new DataMembersExample();
+		
+//		obj.accept(12, 12);
+//		obj.display();
+
+//		System.out.println(DataMembersExample.a=10);
+
+//		DataMembersExample.accept();
+//		DataMembersExample.display();
+
+//		obj.accept();
+//		obj.display();
+	}
+
+}
+
+
+/*  Before OOPS........
+ * 
+ * 		class A{
+ * 
+ * 			public static void main(String args[]){
+ * 				int a=10;
+ * 				int b=20;
+ * 
+ * 				int c = a+b;
+ * 
+ * 				System.out.println(c);
+ * 
+ * 			}
+ * 		
+ * 			}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
